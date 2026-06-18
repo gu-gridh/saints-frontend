f50bcc03ab25c75fba1c2c309245c50729123f78
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2026-06-18 15:14:02 +0200

    some updates to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-  <main>
-    Home
-  </main>
+  <div class="info-article">
+    <div id="hero"></div>
+    <h1>Mapping Saints</h1>
+    <h2>Medieval Cults of Saints 
+      <p> in Sweden & Finland</p></h2>
+  </div>
+  
+
 </template>
+
+<style scoped>
+
+</style>
