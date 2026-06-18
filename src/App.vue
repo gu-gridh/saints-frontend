4976f63a3fcd233b922aa0bd8a1b6d8951183548
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2026-06-18 13:24:30 +0200

    added the views for the menu, updated router

--- src/App.vue
+++ src/App.vue
@@ -3,69 +3,37 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <nav class="main-menu">
+    <RouterLink to="/" class="main-menu-link">Home</RouterLink>
+    <RouterLink to="/introduction" class="main-menu-link">Introduction</RouterLink>
+    <RouterLink to="/explore" class="main-menu-link">Explore</RouterLink>
+    <RouterLink to="/project" class="main-menu-link">Project</RouterLink>
+    <RouterLink to="/bibliography" class="main-menu-link">Bibliography</RouterLink>
+  </nav>
   <RouterView />
+  <div id="footer">
+  </div>  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.main-menu {
+  padding: 0 0 40px;
+  font-size: 25px;
+  font-weight: 200;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  position: relative;
+  z-index: 10;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.main-menu-link {
+  padding: 10px 15px 15px 15px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.main-menu-link.router-link-active {
+  background-color: #dc9614;
+  color: #fff;
+  font-weight: 200;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,26 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/introduction',
+      name: 'introduction',
+      component: () => import('../views/IntroView.vue'),
+    },
+    {
+      path: '/explore',
+      name: 'explore',
+      component: () => import('../views/ExploreView.vue'),
+    },
+    {
+      path: '/project',
+      name: 'project',
+      component: () => import('../views/ProjectView.vue'),
+    },
+    {
+      path: '/bibliography',
+      name: 'bibliography',
+      component: () => import('../views/BiblioView.vue'),
+    },
   ],
 })
 
