6239b8a3add9285ff1f5ed69339ca58483682e83
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2026-06-22 13:27:49 +0200

    added splitpane, added watcher for screen size

---
+++ src/views/DesktopView.vue
@@ -0,0 +1,19 @@
+<template>
+    <splitpanes class="default-theme">
+        <pane size="50%">
+            <Map/>
+        </pane>
+        <pane size="50%">
+            <ExplorePlaces/>
+        </pane>
+    </splitpanes>
+</template>
+
+<script setup>
+import ExplorePlaces from '@/components/ExplorePlaces.vue';
+import Map from '@/components/Map.vue';
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+
+
+</script>
