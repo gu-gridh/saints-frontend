f71e169325ec0d9ad520614677c17258c08fad3d
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2026-06-22 12:00:31 +0200

    Deleted base files. Added Explore menu

---
+++ src/components/ExploreMenu.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="explore-type-menu" @click="startMap">
+    <router-link to="/explore/places" class="item type-places">
+      <Square class="img-container">
+        <img src="./assets/places.jpg" />
+      </Square>
+      <div class="label">Places</div>
+    </router-link>
+
+    <router-link to="/explore/saints" class="item type-saints">
+      <Square class="img-container">
+        <img src="./assets/saints.jpg" />
+      </Square>
+      <div class="label">Saints</div>
+    </router-link>
+
+    <router-link to="/explore/cults" class="item type-cults">
+      <Square class="img-container">
+        <img src="./assets/manifestations.jpg" />
+      </Square>
+      <div class="label">Manifestations</div>
+    </router-link>
+
+    <router-link to="/explore/people" class="item type-people">
+      <Square class="img-container">
+        <img src="./assets/people.jpg" />
+      </Square>
+      <div class="label">People</div>
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import Square from './Square.vue'
+
+const startMap = () => {
+  //set map center to the default one
+}
+</script>
+
+<style lang="scss" scoped>
+#explore-type-menu {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 2em;
+}
+.item {
+  width: 21%;
+  max-width: 8.2rem;
+  padding: 0.4rem;
+  color: inherit;
+}
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  mix-blend-mode: luminosity;
+
+  .type-saints & {
+    filter: brightness(1.5);
+  }
+  .type-people & {
+    filter: brightness(1.7);
+  }
+}
+.img-container {
+  border-radius: 50%;
+  background-color: black;
+  overflow: hidden;
+}
+.item.router-link-active {
+  color: #DC9614;
+  .img-container {
+    background-color: #DC9614;
+  }
+}
+.item .label {
+  font-size: 90%;
+  text-transform: uppercase;
+  text-align: center;
+  margin: 0.5rem 0 0;
+  word-wrap: break-word;
+  line-height: 1.15;
+}
+</style>
