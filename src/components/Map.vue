a544725eb113416ecab78de55a1078be6337fec5
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2026-06-25 10:56:58 +0200

    Markers for places in place

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -27,6 +27,7 @@ import DateSlider from '@/components/DateSlider.vue'
 import { map as fetchMap } from '@/assets/db'
 import { mapCenter as defaultMapCenter } from '@/assets/config'
 import { buildMapParams, onFeatureClick } from '@/assets/query'
+import { placeIcon } from '@/assets/map'
 
 const store = useSaintsStore()
 const router = useRouter()
@@ -110,7 +111,7 @@ async function createQueryLayer() {
   return L.geoJSON(geojson, {
     pointToLayer(feature, latlng) {
       return L.marker(latlng, {
-        icon: createDefaultIcon(),
+        icon: placeIcon(feature),
       })
     },
 
@@ -350,5 +351,10 @@ main {
   border: 5px solid rgba(180, 180, 180, 0.6);
   border-top-color: rgba(0, 0, 0, 0.6);
   animation: spinner 0.6s linear infinite;
+}
+
+.leaflet-shape-icon {
+  background: transparent;
+  border: none;
 }
 </style>
