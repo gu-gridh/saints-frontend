3a624260b9739f282f27d6edda6abddd47925893
j4lib <julia.beck@lir.gu.se>
2026-08-18 10:37:19 +0200

    Turn back time again.

--- src/views/CmsPageView.vue
+++ src/views/CmsPageView.vue
@@ -1,6 +1,19 @@
+<template>
+  <main v-if="page">
+    <CmsContent
+      :page="page"
+      :home-heading="props.slug === 'home'"
+    />
+  </main>
+
+  <p v-else-if="loading">Loading...</p>
+  <p v-else-if="error">Could not load text.</p>
+</template>
+
 <script setup>
 import { computed } from 'vue'
 import { useCmsPage } from '@/composables/useCmsPage'
+import CmsContent from '@/components/CmsContent.vue'
 
 const props = defineProps({
   slug: {
@@ -12,21 +25,10 @@ const props = defineProps({
 const { page, loading, error } = useCmsPage(computed(() => props.slug))
 </script>
 
-<template>
-    <main>
-      <div v-if="page.show_banner" id="hero"></div>
-      <h1>{{ page.title }}</h1>
-  
-      <template v-for="block in page.body" :key="block.id">
-        <h2 v-if="block.type === 'heading'">
-          {{ block.value }}
-        </h2>
-  
-        <div
-          v-else-if="block.type === 'text'"
-          v-html="block.value"
-        />
-      </template>
-    </main>
-  </template>
+<style scoped>
 
+main {
+  padding-bottom: 4rem;
+}
+
+</style>
